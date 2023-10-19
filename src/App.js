5e6c41d1735224119c03088c9cc46a8334d5a2e4
70ni <4thedevelopments@gmail.com>
2023-10-19 09:47:53 +0530

    new branch

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import logo from "./logo.svg";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
-import Navigation from "./Section/Navigation/Navigation";
-import Brief from "./Section/Brief/Brief";
-import Achieve from "./Section/Achievements/Achieve";
-import Case from "./Section/Case/Case";
-import Projects from "./Section/Projects/Projects";
+
+
 import Clients from "./Section/Clients/Clients";
 import Feedback from "./Section/Feedback/Feedback";
 import Footer from "./Section/Footer/Footer";
