2ff3c0afd4fe0fdff91cf8de328fc90499d9bd42
70ni <4thedevelopments@gmail.com>
2023-10-19 11:52:33 +0530

    Processpage, casepro, - mobile view fixed

--- src/Pages/Landing page/Land.jsx
+++ src/Pages/Landing page/Land.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState,useEffect } from "react";
 import Achieve from "../../Section/Achievements/Achieve";
 import Brief from "../../Section/Brief/Brief";
 import Capabilities from "../../Section/Capabilities/Capabilities";
@@ -7,27 +7,36 @@ import CaseBanner from "../../Section/CaseStudy/CaseBanner";
 import Clients from "../../Section/Clients/Clients";
 import Feedback from "../../Section/Feedback/Feedback";
 import Footer from "../../Section/Footer/Footer";
+import LoadingScreen from "../../Section/LoadingScreen/LoadingScreen";
 import IntoSec from "../../Section/Intro/IntoSec";
 import Projects from "../../Section/Projects/Projects";
 import Casestudy from "../Casestudy/Casestudy";
-
 function Land() {
+  const [theme, settheme] = useState();
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     function setTheme(themeName) {
-      document.documentElement.className = themeName;
+      settheme((document.documentElement.className = themeName));
     }
     setTheme("theme-dark");
-  }, []);
+    setTimeout(() => {
+      setTimeout(() => setLoading(false), 3000);
+    }, []);
+    console.log(theme);
+  }, [theme]);
   return (
     <div className="landwrapper">
-      <IntoSec />
-      <Brief />
-      <Achieve />
-      {/* <Projects /> */}
-      {/* <CaseBanner /> */}
-      <Capabilities />
-      {/* <Clients /> */}
-      {/* <Feedback /> */}
+         { loading ? <LoadingScreen />:
+      <>
+        <IntoSec />
+        <Brief />
+        <Achieve />
+        {/* <Projects /> */}
+        {/* <CaseBanner /> */}
+        <Capabilities />
+        {/* <Clients /> */}
+        {/* <Feedback /> */}
+      </>}
     </div>
   );
 }
---
+++ src/Section/LoadingScreen/LoadingScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoadingScreen() {
+  return (
+    <div className='Header' style={{display:'flex',justifyConent:'center',alignItems:'center',height:'100vh'}}>Loading ...</div>
+  )
+}
+
+export default LoadingScreen;
