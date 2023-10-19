a4a16fcfa2a7964141fd88a7cfc2cbf735985acd
70ni <4thedevelopments@gmail.com>
2023-10-19 09:33:24 +0530

    removed unwanted files

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import logo from "./logo.svg";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
-import Navigation from "./Section/Navigation/Navigation";
-import Brief from "./Section/Brief/Brief";
-import Achieve from "./Section/Achievements/Achieve";
-import Case from "./Section/Case/Case";
-import Projects from "./Section/Projects/Projects";
+
 import Clients from "./Section/Clients/Clients";
 import Feedback from "./Section/Feedback/Feedback";
 import Footer from "./Section/Footer/Footer";
-import Capabilities from "./Section/Capabilities/Capabilities";
 import Casestudy from "./Pages/Casestudy/Casestudy";
 import CasePro from "./Pages/Cases/CasePro";
 import Process from "./Pages/Process/Process";
