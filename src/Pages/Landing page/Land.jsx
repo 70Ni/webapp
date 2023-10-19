37d9c4804072bf1f81c486083d9bf86a97d3f8f5
70ni <4thedevelopments@gmail.com>
2023-10-19 10:03:49 +0530

    changes to branch 3

--- src/Pages/Landing page/Land.jsx
+++ src/Pages/Landing page/Land.jsx
@@ -3,10 +3,7 @@ import { useEffect } from "react";
 import Achieve from "../../Section/Achievements/Achieve";
 import Brief from "../../Section/Brief/Brief";
 import Capabilities from "../../Section/Capabilities/Capabilities";
-import CaseBanner from "../../Section/CaseStudy/CaseBanner";
-import Clients from "../../Section/Clients/Clients";
-import Feedback from "../../Section/Feedback/Feedback";
-import Footer from "../../Section/Footer/Footer";
+
 import IntoSec from "../../Section/Intro/IntoSec";
 import Projects from "../../Section/Projects/Projects";
 import Casestudy from "../Casestudy/Casestudy";
